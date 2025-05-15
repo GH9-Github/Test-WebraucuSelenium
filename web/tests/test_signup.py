ce91282410c66399a8393220485ec0dbcd11ebe6
hung100097 <herobrinoob@gmail.com>
2025-05-15 21:03:47 +0700

    update

--- web/tests/test_signup.py
+++ web/tests/test_signup.py
@@ -17,7 +17,7 @@ class SignupTest(unittest.TestCase):
         """TC_DK_01-Đăng ký thành công với thông tin hợp lệ (họ, tên, email, mật khẩu hợp lệ)"""
         new_email = f"user{int(time.time())}@gmail.com"
         new_password = "123456"
-        self.signupPage.SignUp("Phuc","Van",new_email,new_password,new_password)
+        self.signupPage.SignUp("Hung","Nguyen",new_email,new_password,new_password)
         is_login_visible = self.loginPage.verify_login_page_visible()
         result = self.signupPage.get_result_signup(is_login_visible)
         Excel.write_data_to_excel(new_email, new_password, result)
@@ -25,29 +25,29 @@ class SignupTest(unittest.TestCase):
 
     def test_signup_fail_email_name(self):
         """TC_DK_04-Đăng ký thất bại khi email không hợp lệ"""
-        new_email = "phuc"
+        new_email = "hung"
         new_password = "123456"
-        self.signupPage.SignUp("Phuc","Van", new_email, new_password, new_password)
+        self.signupPage.SignUp("Hung","Nguyen", new_email, new_password, new_password)
         self.signupPage.Verify_error_email()
     
     def test_signup_fail_password(self):
         """TC_DK_05-Đăng ký thất bại khi mật khẩu không hợp lệ"""
         new_email = f"user{int(time.time())}@gmail.com"
         new_password = "123"
-        self.signupPage.SignUp("Phuc","Van",new_email,new_password,new_password)
+        self.signupPage.SignUp("Hung","Nguyen",new_email,new_password,new_password)
         self.signupPage.verify_error_signup("Vui lòng nhập mật khẩu chỉ có số và văn bản và ít nhất 6 ký tự!")
     
     def test_signup_fail_confirm_pass(self):
         """TC_DK_06-Đăng ký thất bại khi xác nhận mật khẩu không khớp"""
         new_email = f"user{int(time.time())}@gmail.com"
         new_password = "123456"
-        self.signupPage.SignUp("Phuc","Van",new_email,new_password,"123678")
+        self.signupPage.SignUp("Hung","Nguyen",new_email,new_password,"123678")
         self.signupPage.verify_error_signup("Mật khẩu phải khớp!")
         
     def test_signup_fail_all(self):
         """TC_DK_07-Đăng ký thất bại khi tất cả thông tin không hợp lệ"""
         self.signupPage.SignUp("","","","","")
-        self.signupPage.verify_error_signup("Vui lòng nhập email hợp lệ!")
+        self.signupPage.verify_error_signup("Vui lòng nhập họ của bạn!")
 
     @classmethod
     def tearDownClass(cls):
